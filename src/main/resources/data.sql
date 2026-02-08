795e074ab5cd333ee5654cd5f0ac67de1b38b8bc
patorma <pocontreras@ing.ucsc.cl>
2026-02-07 22:59:37 -0300

    se agregó sentencia sql a data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (nombres,apellidos,comuna,telefono,email,password,role) VALUES ('Alex','Guzman','Santiago','432469265','alex@gmail.com','$2a$10$NZuI0GMZFJClfSLRoSftiuRByHPMhFBA05PT3VMv5mEiUXz6b5qxa','ADMIN')
     ON CONFLICT (email) DO NOTHING;
+
+SELECT * FROM asignaciones_de_estudiantes;
