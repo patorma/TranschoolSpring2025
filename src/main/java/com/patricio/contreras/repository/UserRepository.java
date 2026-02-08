4d0a2f2ad9eaa481f7c606a8322ecd9c0083193c
patorma <pocontreras@ing.ucsc.cl>
2026-02-07 23:05:55 -0300

    se agrega sentencia sql para ver transportistas  con furgon

--- src/main/java/com/patricio/contreras/repository/UserRepository.java
+++ src/main/java/com/patricio/contreras/repository/UserRepository.java
@@ -26,4 +26,9 @@ public interface UserRepository extends JpaRepository<User,Long>{
               "FROM Users  WHERE role='TRANSPORTISTA' AND id NOT IN" +
               " (SELECT usuario_transportista_id  FROM Furgones)",nativeQuery = true)
       Page<User> transportistasSinFurgon(Pageable pageable);
+
+	@Query(value = "SELECT id, nombres,apellidos, comuna,email,role,password,telefono" +
+			"FROM Users  WHERE role='TRANSPORTISTA' AND id IN" +
+			"(SELECT usuario_transportista_id  FROM Furgones)",nativeQuery = true)
+	Page <User> transportistasConFurgon(Pageable pageable);
 }
